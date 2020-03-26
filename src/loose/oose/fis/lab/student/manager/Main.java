dbeb61edf983f8a853d2a918afecc573a900c052
iuliadrgn <62391354+iuliadrgn@users.noreply.github.com>
2020-03-26 14:54:24 -0700

    Update Main.java

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -8,7 +8,6 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
@@ -18,7 +17,9 @@ public class Main extends Application {
     }
 
 
+
     public static void main(String[] args) {
         launch(args);
     }
 }
+
